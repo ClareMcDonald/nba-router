70b79e85a80f2ebe3fa6b3014c5ba2ac3c3f3715
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-05-09 11:13:07 -0700

    mapped through characters in CharacterList

--- src/views/Characters/CharacterList.jsx
+++ src/views/Characters/CharacterList.jsx
@@ -15,27 +15,38 @@ export default function CharacterList() {
       const url =
         currentStatus === 'all' || !currentStatus
           ? 'https://rickandmortyapi.com/api/character'
-          : `https://rickandmortyapi.com/api/character?status=${currentStatus}`
+          : `https://rickandmortyapi.com/api/character?status=${currentStatus}`;
       
       const res = await fetch(url);
-      const results = await res.json();
+      const { results } = await res.json();
 
       setCharacters(results)
       setLoading(false);
     }
     fetchCharacters();
-  }, []);
-
+  }, [location.search]);
+  
+  console.log(characters)
   return (
     <>
       <h1>Rick and Morty: Characters</h1>
     {
       loading
         ? <img alt='loading spinner' src='/spinner.gif'/>
-        : <div>
-            <form>
-            </form>  
-          </div>
+          : <>
+            <div>
+              <form>
+              </form>  
+            </div>
+            {characters.map((character) => (
+              <div key={character.id}>
+              <h3>{character.name}</h3>
+              <img src={character.image}/>
+              <p>{character.status}</p>
+              <p>{character.species}</p>
+              </div>
+            ))}
+            </>
     }
     </>
   )
