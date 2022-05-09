5beb244a6366b17e12e59dc23dd56f075699aeec
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-05-09 09:58:51 -0700

    added switch, routes, and imports to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import { Route, Switch } from "react-router-dom";
+import CharacterList from "./views/Characters/CharacterList";
+import CharacterDetail from "./views/Characters/CharacterDetail";
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Switch>
+      <Route path='/characters/:id'>
+        <CharacterDetail />
+      </Route>
+      <Route path='/characters'>
+        <CharacterList />
+      </Route>
+    </Switch>
+  );
 }
