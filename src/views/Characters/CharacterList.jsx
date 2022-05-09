09701dfe6cfb9742cae490f9175ac9de342c9829
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-05-09 11:01:50 -0700

    wrote character list useEffect

--- src/views/Characters/CharacterList.jsx
+++ src/views/Characters/CharacterList.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
 export default function CharacterList() {
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
+  const location = useLocation();
+  const status = new URLSearchParams(location.search).get('status') ?? 'all';
+    
+  useEffect(() => {
+    async function fetchCharacters() {
+      setLoading(true);
+      const currentStatus = new URLSearchParams(location.search).get('status');
+      const url =
+        currentStatus === 'all' || !currentStatus
+          ? 'https://rickandmortyapi.com/api/character'
+          : `https://rickandmortyapi.com/api/character?status=${currentStatus}`
+      
+      const res = await fetch(url);
+      const results = await res.json();
+
+      setCharacters(results)
+      setLoading(false);
+    }
+    fetchCharacters();
+  }, []);
+
   return (
-    <div>CharacterList</div>
+    <>
+      <h1>Rick and Morty: Characters</h1>
+    {
+      loading
+        ? <img alt='loading spinner' src='/spinner.gif'/>
+        : <div>
+            <form>
+            </form>  
+          </div>
+    }
+    </>
   )
 }
